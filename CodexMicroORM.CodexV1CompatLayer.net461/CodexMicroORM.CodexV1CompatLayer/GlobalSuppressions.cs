cde8d9cad2fad578503b7911946766bc8aad032f
joel <admin@codexframework.com>
2021-08-03 06:38:24 -0700

    no longer needed

--- CodexMicroORM.CodexV1CompatLayer.net461/CodexMicroORM.CodexV1CompatLayer/GlobalSuppressions.cs
+++
@@ -1,9 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "Code checked", Scope = "module")]
-[assembly: SuppressMessage("Usage", "CA2235:Mark all non-serializable fields", Justification = "Checked", Scope = "module")]
